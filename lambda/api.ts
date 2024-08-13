2b04b5e2b919acebdb49f1811f0512d38c9e91f7
Aga Ariesta Wibowo <itu.aga42@gmail.com>
2024-08-13 22:41:50 +0700

    tweak: cors

--- lambda/api.ts
+++ lambda/api.ts
@@ -22,6 +22,7 @@ const makeRequest = async <Res>(
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "content-type,authorization",
       },
       mode: "no-cors",
     });
