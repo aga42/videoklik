bf979068b451e75d338fbff6b149c8c2d7319c8e
Aga Ariesta Wibowo <itu.aga42@gmail.com>
2024-08-14 18:13:33 +0700

    add: monetag ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="favicon.ico" />
         <meta name="monetag" content="d5d404b079fc8fae3c2c58a8ba005ae6"></meta>
-        <script src="https://alwingulla.com/88/tag.min.js" data-zone="88662" async data-cfasync="false"></script>
+        {/* <script src="https://alwingulla.com/88/tag.min.js" data-zone="88662" async data-cfasync="false"></script> */}
+        <script src="https://atshroomisha.com/act/files/tag.min.js?z=7892610" data-cfasync="false" async></script>
       </head>
       <body>
       <Suspense fallback={<div>Loading...</div>}>
