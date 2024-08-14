387003fec3dec39733effc7e32f6cdb8755c48f4
Aga Ariesta Wibowo <itu.aga42@gmail.com>
2024-08-14 21:59:08 +0700

    fix: ads.js

--- app/script/ads.js
+++ app/script/ads.js
@@ -1,13 +1,13 @@
-(function bro(d,z,s)
+(function(d,z,s)
 {
     s.src='https://'+d+'/400/'+z;
     try{
-      (document.body||document.documentElement).appendChild(s)
+      (document.body||document.documentElement).appendChild(s);
     }catch(e){
 
     }
 })
-('waitheja.net',7892576,document.createElement('script'))
+('waitheja.net',7892576,document.createElement('script'));
 
 {/* <script>
   (function(d,z,s){
