1290f1332d7340940360700eb9471a5e946a7768
Aga Ariesta Wibowo <itu.aga42@gmail.com>
2024-08-14 10:43:44 +0700

    tweak: cors

--- lambda/api.ts
+++ lambda/api.ts
@@ -63,7 +63,7 @@ export const renderVideo = async ({
   };
 
   // return makeRequest<RenderMediaOnLambdaOutput>("/api/lambda/render", body);
-  return makeRequest<RenderMediaOnLambdaOutput>("https://ulmmtt5bustyydwjumrpd34w7a0ndtnb.lambda-url.us-east-1.on.aws/", body);
+  return makeRequest<RenderMediaOnLambdaOutput>("https://ulmmtt5bustyydwjumrpd34w7a0ndtnb.lambda-url.us-east-1.on.aws/.", body);
 };
 
 export const getProgress = async ({
@@ -79,5 +79,5 @@ export const getProgress = async ({
   };
 
   // return makeRequest<ProgressResponse>("/api/lambda/progress", body);
-  return makeRequest<ProgressResponse>("https://ulmmtt5bustyydwjumrpd34w7a0ndtnb.lambda-url.us-east-1.on.aws/", body);
+  return makeRequest<ProgressResponse>("https://ulmmtt5bustyydwjumrpd34w7a0ndtnb.lambda-url.us-east-1.on.aws/.", body);
 };
