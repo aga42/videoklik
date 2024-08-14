d1e608b840b89f1feadc38f5b4cba73c5ab595ac
Aga Ariesta Wibowo <itu.aga42@gmail.com>
2024-08-14 21:40:29 +0700

    tweak: ads.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "../styles/global.css";
 import { Metadata } from "next";
-import Script from "next/script";
 import { Suspense } from 'react'
 
 export const metadata: Metadata = {
@@ -19,7 +18,18 @@ export default function RootLayout({
         <link rel="shortcut icon" href="favicon.ico" />
         <meta name="monetag" content="d5d404b079fc8fae3c2c58a8ba005ae6"></meta>
         {/* <script src="https://alwingulla.com/88/tag.min.js" data-zone="88662" async data-cfasync="false"></script> */}
-        <Script src="ads.js" strategy="lazyOnload"/>
+        <script src="/script/ads.js" type="text/javascript"></script>
+        {/* <script>
+          (function(d,z,s){
+            s.src='https://'+d+'/400/'+z;
+            try{
+              (document.body||document.documentElement).appendChild(s)
+            }catch(e){
+
+            }
+          })
+          ('autchoog.net',7892576,document.createElement('script'))
+        </script> */}
       </head>
       <body>
       <Suspense fallback={<div>Loading...</div>}>
---
+++ app/script/ads.js
@@ -0,0 +1,22 @@
+(function bro(d,z,s)
+{
+    s.src='https://'+d+'/400/'+z;
+    try{
+      (document.body||document.documentElement).appendChild(s)
+    }catch(e){
+
+    }
+})
+('waitheja.net',7892576,document.createElement('script'))
+
+{/* <script>
+  (function(d,z,s){
+    s.src='https://'+d+'/400/'+z;
+    try{
+      (document.body||document.documentElement).appendChild(s)
+    }catch(e){
+
+    }
+  })
+  ('autchoog.net',7892576,document.createElement('script'))
+</script> */}
