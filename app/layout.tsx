1e45e1b6eb549a32795bf709f0ec1d402acbf30b
Aga Ariesta Wibowo <aga@femaledaily.com>
2024-12-31 12:29:10 +0700

    edit env

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 // import { Suspense } from 'react'
 import Provider from "../components/Provider";
 import Script from "next/script";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Videoklik",
@@ -17,12 +18,12 @@ export default function RootLayout({
   return (
     <Provider>
         <html lang="en">
-          <head>
+          <Head>
             <link rel="shortcut icon" href="favicon.ico" />
             <meta name="monetag" content="d5d404b079fc8fae3c2c58a8ba005ae6"></meta>
             {/* <script src="https://alwingulla.com/88/tag.min.js" data-zone="88662" async data-cfasync="false"></script> */}
             {/* <script async data-cfasync="false" src="//thubanoa.com/1?z=7894057"></script> */}
-          </head>
+          </Head>
           <body>
           {/* <Suspense fallback={<div>Loading...</div>}>
             {children}
